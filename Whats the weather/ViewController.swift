ce4d2e1d29fd0278cb7fdecbd222b40e9e7a0b3b
Arkadiusz Stankiewicz <arkadiuszstankiewicz@MacBook-Pro-test.local>
2017-10-07 15:47:20 +0200

    complete app

--- Whats the weather/ViewController.swift
+++ Whats the weather/ViewController.swift
@@ -9,17 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func getWeather(_ sender: Any) {
+        
+        if let url = URL(string: "https://www.weather-forecast.com/locations/" + cityTextField.text!.replacingOccurrences(of: " ", with: "-").replacingOccurrences(of: "ń", with: "n") + "/forecasts/latest"){
+        let request = NSMutableURLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            data, response, error in
+            var message = ""
+            if error != nil{
+                print(error)
+            }
+            else{
+                if let unwrappedData = data{
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    var stringSeperator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                    if let contentArray = dataString?.components(separatedBy: stringSeperator){
+                        if contentArray.count > 1{
+                            stringSeperator = "</span>"
+                            
+                            let newContentArray = contentArray[1].components(separatedBy: stringSeperator)
+                            if newContentArray.count > 1{
+                                message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "^")
+                            }
+                        }
+                    }
+                }
+            }
+            if message == ""{
+                message = "The weather there couldn't be found. Please try again."
+            }
+            DispatchQueue.main.sync(execute: {
+                self.resultLabel.text = message
+            })
+        }
+        task.resume()
+        }
+        else{
+            resultLabel.text = "The weather there couldn't be found. Please try again"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
